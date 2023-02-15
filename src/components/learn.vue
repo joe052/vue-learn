6969f23f6a4cb32526af79df2236303b45b4bf83
joe052 <joekaranjasenior52@gmail.com>
2023-02-16 01:27:43 +0300

    message

---
+++ src/components/learn.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "The last of us",
+      author: "John cage",
+      age: 0,
+    };
+  },
+
+  methods: {
+    increment() {
+      this.age++;
+    },
+  },
+
+  mounted() {
+    console.log(`The guy's age is ${this.count}.`);
+  },
+};
+</script>
+
+<template>
+  <p>{{ title }}-{{ author }}-{{ age }}</p>
+  <button @click="increment">Lie</button>
+</template>
