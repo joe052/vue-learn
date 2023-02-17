c1b0ffa8ca67e4a8be8f0216499ce04dc8ac2dd8
joe052 <joekaranjasenior52@gmail.com>
2023-02-17 16:26:20 +0300

    message

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
+<template>
+  <!-- <learn /> -->
+  <RouterView />
+</template>
+
 <script setup>
 import learn from "./components/learn.vue";
 import SignUp from "./components/SignUp.vue";
+import { RouterView } from "vue-router";
 </script>
-
-<template>
-  <!-- <learn /> -->
-  <SignUp />
-</template>
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,6 +1,9 @@
 <template>
-  <img src="../assets/shopify.png" />
-  <h1>Sign Up</h1>
+  <!-- <img src="../assets/shopify.png" />
+  <h1>Sign Up</h1> -->
+  <div class="justify-center flex bg-blue-300 items-center h-screen">
+    <div class="text-4xl">Hello Welcome to Tailwind Css</div>
+  </div>
 </template>
 
 <script>
