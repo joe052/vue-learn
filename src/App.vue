fc98266ef1f834a724740e6725ee9c2f6b30004e
joe052 <joekaranjasenior52@gmail.com>
2023-02-17 21:18:55 +0300

    message

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
   <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
-    <!-- <learn /> -->
-    <!-- <SiteNav /> -->
-    <!-- <RouterView /> -->
+    <SiteNav />
+    <RouterView />
   </div>
 </template>
 
 <script setup>
-// import { RouterView } from "vue-router";
-// import learn from "./components/learn.vue";
-// import SiteNav from "./components/SiteNav.vue";
-// import SignUp from "./components/SignUp.vue";
+import { RouterView } from "vue-router";
+import SiteNav from "./components/SiteNav.vue";
 </script>
 
 <style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 const routes = [
     {
         path: "/",
-        name: "Home",
+        name: "home",
         // component: () => import("../vue"),
     }];
 
