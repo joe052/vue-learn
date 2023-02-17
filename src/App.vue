0ddac2649d5b837795620c789210d2497517c27b
joe052 <joekaranjasenior52@gmail.com>
2023-02-17 11:48:01 +0300

    message

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,9 @@
 <script setup>
 import learn from "./components/learn.vue";
+import SignUp from "./components/SignUp.vue";
 </script>
 
-<template> 
-  <learn />
+<template>
+  <!-- <learn /> -->
+  <SignUp />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,8 @@
+<template>
+  <img src="../assets/shopify.png" />
+  <h1>Sign Up</h1>
+</template>
+
+<script>
+export default {};
+</script>
