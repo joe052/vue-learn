b56fd61082b28e99e747bda3e79c3e8b667957d2
joe052 <joekaranjasenior52@gmail.com>
2023-02-17 20:41:24 +0300

    message

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
     <!-- <learn /> -->
-    <SiteNav />
+    <!-- <SiteNav /> -->
     <!-- <RouterView /> -->
   </div>
 </template>
 
 <script setup>
-import { RouterView } from "vue-router";
-import learn from "./components/learn.vue";
-import SiteNav from "./components/SiteNav.vue";
-import SignUp from "./components/SignUp.vue";
+// import { RouterView } from "vue-router";
+// import learn from "./components/learn.vue";
+// import SiteNav from "./components/SiteNav.vue";
+// import SignUp from "./components/SignUp.vue";
 </script>
 
-<style>
-@import url("./assets/tailwind.css");
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from "vue";
 import router from "./router";
 import App from "./App.vue";
-import "./index.css";
+import "./assets/styles/tailwind.css";
+// import "./index.css";
 // import "./style.css";
-// import "./assets/tailwind.css";
 const app = createApp(App);
 app.use(router);
 app.mount("#app");
