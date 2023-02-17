aac8b04edb077e9b4d68ed161d416b64f286f0fc
joe052 <joekaranjasenior52@gmail.com>
2023-02-18 01:25:36 +0300

    message

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
+/**Define routes here */
 const routes = [
-    {
-        path: "/",
-        name: "home",
-        // component: () => import("../vue"),
-    }];
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+];
 
+/**Initialize here */
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input
+        type="text"
+        placeholder="Search for a city or state"
+        class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
+      />
+    </div>
+  </main>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
