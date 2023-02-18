70d80b6ab5d1ec4959a1c7fec17d5036689a6b0d
joe052 <joekaranjasenior52@gmail.com>
2023-02-18 04:23:52 +0300

    message

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,6 +8,17 @@
         placeholder="Search for a city or state"
         class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
       />
+      <ul
+        class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+      >
+        <li
+          v-for="searchResult in mapboxSearchResults"
+          :key="searchResult.id"
+          class="py-2 cursor-pointer"
+        >
+          {{ searchResult.place_name }}
+        </li>
+      </ul>
     </div>
   </main>
 </template>
